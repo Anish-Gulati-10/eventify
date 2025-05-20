97d5eeb5507823d498713f164bf6ea5281367755
Anish-Gulati-10 <theanishgulati@gmail.com>
2025-05-20 16:27:35 +0530

    Fix spelling

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -30,7 +30,7 @@ export default function Navbar() {
         <div className="flex justify-between h-16 items-center">
           {/* Logo */}
           <Link href="/" className="text-xl font-bold text-primary">
-            EventManager
+            Eventify
           </Link>
 
           {/* Desktop menu */}
